19bea1ba344e27ce379996618150c72041569a31
Qazim Rama <qazimrama@live.com>
2017-03-08 12:51:21 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function calculate() {
+
+	var number = document.getElementById("number").value;
+
+	if (number == "" || number == 0) {
+		window.alert("Please type your corect age!");
+		return;
+	}
+	var date = new Date().getFullYear();
+
+	var birthyear = number - date;
+
+	document.getElementById("year").style.display = "block";
+	document.getElementById("year").innerHTML = "Your birth is " + birthyear;
+	
+}
+
+//document.getElementById("year").style.display = "none";
+document.getElementById("button").onClick = function() { calculate(); };
